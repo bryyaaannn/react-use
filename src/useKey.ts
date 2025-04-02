c590b09a107ae15b560b5979276592bfb9c52c89
Bryan Wang <bryan.wang@zoom.us>
2025-04-02 15:31:42 +0800

    update usekey.ts

--- src/useKey.ts
+++ src/useKey.ts
@@ -1,33 +1,38 @@
-import { DependencyList, useMemo } from 'react';
-import useEvent, { UseEventOptions, UseEventTarget } from './useEvent';
-import { noop } from './misc/util';
+import { DependencyList, useMemo } from "react";
+import useEvent, { UseEventOptions, UseEventTarget } from "./useEvent";
+import { noop } from "./misc/util";
 
 export type KeyPredicate = (event: KeyboardEvent) => boolean;
-export type KeyFilter = null | undefined | string | ((event: KeyboardEvent) => boolean);
+export type KeyFilter =
+  | null
+  | undefined
+  | string
+  | ((event: KeyboardEvent) => boolean);
 export type Handler = (event: KeyboardEvent) => void;
 
 export interface UseKeyOptions<T extends UseEventTarget> {
-  event?: 'keydown' | 'keypress' | 'keyup';
+  event?: "keydown" | "keypress" | "keyup";
   target?: T | null;
   options?: UseEventOptions<T>;
 }
 
 const createKeyPredicate = (keyFilter: KeyFilter): KeyPredicate =>
-  typeof keyFilter === 'function'
+  typeof keyFilter === "function"
     ? keyFilter
-    : typeof keyFilter === 'string'
-    ? (event: KeyboardEvent) => event.key === keyFilter
-    : keyFilter
-    ? () => true
-    : () => false;
+    : typeof keyFilter === "string"
+      ? (event: KeyboardEvent) => event.key === keyFilter
+      : keyFilter
+        ? () => true
+        : () => false;
 
+// test comment
 const useKey = <T extends UseEventTarget>(
   key: KeyFilter,
   fn: Handler = noop,
   opts: UseKeyOptions<T> = {},
-  deps: DependencyList = [key]
+  deps: DependencyList = [key],
 ) => {
-  const { event = 'keydown', target, options } = opts;
+  const { event = "keydown", target, options } = opts;
   const useMemoHandler = useMemo(() => {
     const predicate: KeyPredicate = createKeyPredicate(key);
     const handler: Handler = (handlerEvent) => {
